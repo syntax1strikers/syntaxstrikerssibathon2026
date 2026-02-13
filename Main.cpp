0d42fa20ac3956247285c298f7cda51b6a28da44
syntax1strikers <nimra.iftkhar00@gmail.com>
2026-02-13 03:09:33 -0800

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+const int TOTAL=50;
+const int MAX_QUESTION=30;
+
+string name[TOTAL];
+string gender[TOTAL];
+string hair[TOTAL];
+string height[TOTAL];
+string beard[TOTAL];
+string personality[TOTAL];
+string sportsperson[TOTAL];
+string singer[TOTAL];
+string language[TOTAL];
+string married[TOTAL];
+string glasses[TOTAL];
+string kids[TOTAL];
+string overseas[TOTAL];
+string old[TOTAL];
+string dramas[TOTAL];
